dcd7f7848c4f95081afc4a03e6361189d89138fe
BMonesh <141898445+BMonesh@users.noreply.github.com>
2025-02-14 23:04:42 +0530

    STT

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import PostureTester from "./Pose";
+// import PostureTester from "./Pose";
+import STT from "./Components/STT";
 
 function App() {
   return (
     <>
-      <PostureTester />
+      {/* <PostureTester /> */}
+      <STT />
     </>
   );
 }
