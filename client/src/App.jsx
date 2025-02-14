394c2077c8a8a92563174d4540b610a9c33a8b0f
Kishore <ki225ku@gmail.com>
2025-02-14 21:30:24 +0530

    added good things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import PostureTester from "./Pose";
+import Questionnaire from "./Components/UserInfoQuestions/Questions";
 
 function App() {
   return (
     <>
-      <PostureTester />
+      {/* <PostureTester /> */}
+      <Questionnaire />
+      
     </>
   );
 }
