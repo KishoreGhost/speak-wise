7fb8486dca3e294ba3aee8bfe9f0af6a194caf52
Shaswath <shaswathgiridhran@gmail.com>
2025-02-14 21:09:04 +0530

    client side login and sign up done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PostureTester from "./Pose";
+import Login from "./Components/Login"
+import Signup from "./Components/Signup";
 
 function App() {
   return (
-    <>
-      <PostureTester />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/posture-tester" element={<PostureTester />} />
+      </Routes>
+    </Router>
   );
 }
 
